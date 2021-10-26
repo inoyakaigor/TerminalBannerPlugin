7da0d9c4355ea315eb67f1f23312f7005e7ed50a
Igor Zvyagintsev <igor.zvjagincev@bostongene.com>
2021-10-26 20:29:35 +0300

    0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# TerminalBannerPlugin
+# Webpack Terminal Banner Plugin
 Webpack plugin for showing banner after all notifications
+
+[![npm Version](https://img.shields.io/npm/v/@inoy/terminal-banner-plugin.svg)](https://www.npmjs.com/package/@inoy/terminal-banner-plugin)
+
+![Terminal banner screenshot](screenshot.png)
+
+## Using
+```js
+// webpack.config.js
+const TerminalBannerPlugin = require('@inoy/terminalbannerplugin')
+// other code
+
+module.exports = {
+    //… rest of your webpack config
+    plugins: [
+        new WebpackNotifier2Plugin(/* check out my other plugin ;-) */),
+        new TerminalBannerPlugin()
+    ]
+}
+```
+
+## Requirements
+Minimal supported is Node 14 and Webpack 5. Node 12 and Webpack 4 should work but I didn't test.
+
+## License
+GPLv3
---
+++ index.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const cp = require('child_process')
+
+class TerminalBannerPlugin {
+    constructor() {
+        this.VERSION = this.getVersion()
+        this.BRANCH = this.getBranch()
+        this.folder = path.basename(process.cwd())
+    }
+
+    getVersion = () => cp.execSync('git describe --abbrev=0 --tags').toString().trim()
+    getBranch = () => cp.execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+
+    apply = compiler => {
+        compiler.hooks.done.tap('TerminalBannerPlugin', this.compilationDone)
+    }
+
+    compilationDone = () => {
+        // COLORS https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+        const RED = '\x1b[41m'
+        const GREEN = '\x1b[32m'
+        const BLUE = '\x1b[44m'
+        const NC = '\x1b[0m' // no color
+
+        const str = `⌫ ${this.VERSION} │ ⎇ ${this.BRANCH}`
+
+        setTimeout(() => { //hack:  print after all
+            console.log(`\n${GREEN}┏${'━'.repeat(this.VERSION.length + 4)}┯${'━'.repeat(this.BRANCH.length + 4)}┓`)
+            console.log(`┃ ${str} ┃`)
+            console.log(`┗${'━'.repeat(this.VERSION.length + 4)}┷${'━'.repeat(this.BRANCH.length + 4)}┛${NC}`)
+
+            console.log(`${BLUE} ${this.folder} ${NC}${RED} ${(new Date).toLocaleTimeString()} ${NC}\n`)
+        }, 0)
+    }
+}
+
+module.exports = TerminalBannerPlugin
